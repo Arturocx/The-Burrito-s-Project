235faf4303b5c834eea4f386d42fac5f8f8069b3
Samyloko14 <samyberz@outlook.com>
2016-09-26 15:21:44 -0700

    Entities Registro Ajedrez

---
+++ RegistroAjedrez.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities
+{
+    class RegistroAjedrez
+    {
+        public int Id { get; set; }
+        public int IdAlumno { get; set; }
+        public int IdAjedrez { get; set; }
+        public DateTime FechaHoraInicio { get; set; }
+        public DateTime FechaHoraFinal { get; set; }
+    }
+}
