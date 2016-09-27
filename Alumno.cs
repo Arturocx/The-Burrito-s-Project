e666a742cf4ab08df57b57b2cb14b6f18b1d1329
Ana Luisa Magana Salaza <Ana Luisa Magana Salaza>
2016-09-27 12:26:46 -0700

    Entities de Alumno

---
+++ Alumno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities
+{
+    public class Alumno
+    {
+        public int NoControl { get; set; }
+        public string Grupo { get; set; }
+        public string Nombre { get; set; }
+        public byte[] Photo { get; set; }
+        public bool Bloqueado { get; set; }
+
+    }
+}
