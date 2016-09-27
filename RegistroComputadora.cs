9f6de0197da359b83bd111948d5ee6fd4ff1243d
Ana Luisa Magana Salaza <Ana Luisa Magana Salaza>
2016-09-27 12:15:29 -0700

    Entities Registro de Computadoras

---
+++ RegistroComputadora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities
+{
+    public class RegistroComputadora
+    {
+        public int ID { get; set; }
+        public int IDAlumno { get; set; }
+        public int IDComputadora { get; set; }
+        public DateTime FechaHoraInicio  { get; set; }
+        public DateTime FechaHoraFinal { get; set; }
+
+    }
+}
