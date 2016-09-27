5d9fd4e46519440bf0cedde49516c2b5d9ca2da9
Ana Luisa Magana Salaza <Ana Luisa Magana Salaza>
2016-09-27 11:50:34 -0700

    Entities Registro de Libros

---
+++ RegistrodeLibro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Libros
+{
+    public class Registro_de_Libro
+    {
+        public int LibroID { get; set; }
+        public int IDAlumno { get; set; }
+        public DateTime FechadeSalida { get; set; }
+        public DateTime FechaLimite { get; set; }
+        public DateTime FechadeEntrega{ get; set; }
+        public string DetallesdelLibro { get; set; }
+    }
+}
