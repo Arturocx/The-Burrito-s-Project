6a64b441ac4bade4229b5410730bc2965fb319db
Ana Luisa Magana Salaza <Ana Luisa Magana Salaza>
2016-09-27 12:30:45 -0700

    Entities de Strikes

---
+++ Strikes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities
+{
+    public class Strikes
+    {
+        public int ID { get; set; }
+        public int IDAlumno { get; set; }
+        public int IDUsuario { get; set; }
+        public string Motivo { get; set; }
+    }
+}
