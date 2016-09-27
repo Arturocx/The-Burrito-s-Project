d2ef5a196248067df07fb911cdf9815677f3bcd1
Ana Luisa Magana Salaza <Ana Luisa Magana Salaza>
2016-09-27 11:26:19 -0700

    Entities Libros

---
+++ Libro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Libros
+{
+    public class Libro
+    {
+        
+        
+
+        public int LibroID { get; set; }
+        public string Nombre { get; set; }
+        public string Disponible { get; set; }
+        public DateTime FechadePublicacion { get; set; }
+        public byte[] Photo { get; set; }
+        public string Materia { get; set; }
+        public string Detalles { get; set; }
+
+
+    }
+}
