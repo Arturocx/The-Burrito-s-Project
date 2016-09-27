5f63d2705379e244f9ada4c61eebf3dda6f72a9f
Ana Luisa Magana Salaza <Ana Luisa Magana Salaza>
2016-09-27 12:28:54 -0700

    Entities de Usuario

---
+++ Usario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities
+{
+    public class Usario
+    {
+        public string Nombre { get; set; }
+        public string Contrasena { get; set; }
+        public string Username { get; set; }
+        public string APaterno { get; set; }
+        public string AMaterno { get; set; }
+    }
+}
