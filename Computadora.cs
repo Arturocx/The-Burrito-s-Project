9546749ef6e89aed0cfb4ae07c4f8230f027d8c3
Ana Luisa Magana Salaza <Ana Luisa Magana Salaza>
2016-09-27 12:10:11 -0700

    Entities de Computadora

---
+++ Computadora.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities
+{
+    public class Computadora
+    {
+        public int NoCubiculo { get; set; }
+        public string ModeloCompu { get; set; }
+        public string Status { get; set; }
+
+    }
+}
