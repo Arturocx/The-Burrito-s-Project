34ecb5a352add3fe294ff9b6780e755aad3493d9
Samyloko14 <samyberz@outlook.com>
2016-09-27 19:18:44 -0700

    key en ajedrez

---
+++ AppTemplate/App.Entities/RegistroAjedrez.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace App.Entities
+{
+    class RegistroAjedrez
+    {
+        [Key]
+        public int Id { get; set; }
+        public int IdAlumno { get; set; }
+        public int IdAjedrez { get; set; }
+        public DateTime FechaHoraInicio { get; set; }
+        public DateTime FechaHoraFinal { get; set; }
+    }
+}
