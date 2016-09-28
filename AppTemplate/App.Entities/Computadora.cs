b87f94e4ca1a03b9a6b09fd922b13f393fd31064
MikeVargas98 <mikeavt98@gmail.com>
2016-09-27 19:07:31 -0700

    Llave primaria a Computadora

---
+++ AppTemplate/App.Entities/Computadora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace App.Entities
+{
+    public class Computadora
+    {   
+        [Key]
+        public int Id { get; set; }
+        public int NoCubiculo { get; set; }
+        public string ModeloCompu { get; set; }
+        public string Status { get; set; }
+
+    }
+}
