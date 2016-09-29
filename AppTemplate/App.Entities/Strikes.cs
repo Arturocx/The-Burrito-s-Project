cc5a2480362c4ed84db2f1be3c266eee850d58aa
Arturocx <chris-mp@hotmail.com>
2016-09-28 18:16:14 -0700

    Ingreso.Usuario

--- AppTemplate/App.Entities/Strikes.cs
+++ AppTemplate/App.Entities/Strikes.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace App.Entities
 {
     public class Strikes
     {
+        [Key]
         public int ID { get; set; }
         public int IDAlumno { get; set; }
         public int IDUsuario { get; set; }
--- AppTemplate/App.Entities/Usario.cs
+++ AppTemplate/App.Entities/Usario.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace App.Entities
 {
     public class Usario
     {
+        [Key]
         public string Nombre { get; set; }
         public string Contrasena { get; set; }
         public string Username { get; set; }
