b0824748daf112854d9efc03d5f498e6512def55
nukolml <jorgesuperchicogit config --global user.email jorgesuperchico@gmail.com>
2016-09-28 18:16:26 -0700

    Entities Libro

--- AppTemplate/App.Entities/Libro.cs
+++ AppTemplate/App.Entities/Libro.cs
@@ -2,19 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Libros
 {
     public class Libro
     {
-        
-        
-
-        public int LibroID { get; set; }
+           [Key]
+        public int Id { get; set; }
         public string Nombre { get; set; }
         public string Disponible { get; set; }
-        public DateTime FechadePublicacion { get; set; }
-        public byte[] Photo { get; set; }
         public string Materia { get; set; }
         public string Detalles { get; set; }
 
