17f766898114fb745eccd962b3a73122904cc463
mikeavt98 <mikeavt98@gmail.com>
2016-09-28 18:04:18 -0700

    Ingreso.Computadoras

---
+++ AppTemplate/App.Entities/RegistroComputadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace App.Entities
+{
+    public class RegistroComputadora
+    {
+        [Key]
+        public int ID { get; set; }
+        public int IDAlumno { get; set; }
+        public int IDComputadora { get; set; }
+        public DateTime FechaHoraInicio  { get; set; }
+        public DateTime FechaHoraFinal { get; set; }
+    }
+}
