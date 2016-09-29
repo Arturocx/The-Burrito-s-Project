3ee241d834097814dd422506595c4a68265de60b
nukolml <jorgesuperchicogit config --global user.email jorgesuperchico@gmail.com>
2016-09-28 18:30:36 -0700

    Entities IngresoLibro

--- AppTemplate/App.Entities/RegistrodeLibro.cs
+++ AppTemplate/App.Entities/RegistrodeLibro.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Libros
 {
     public class Registro_de_Libro
     {
-        public int LibroID { get; set; }
-        public int IDAlumno { get; set; }
+        [Key]
+        public int Id { get; set; }
+        public int Alumno { get; set; }
         public DateTime FechadeSalida { get; set; }
         public DateTime FechaLimite { get; set; }
         public DateTime FechadeEntrega{ get; set; }
