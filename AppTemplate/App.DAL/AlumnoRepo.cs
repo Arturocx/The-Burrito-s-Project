496e00c052bf8699fcc50e2c866d833b5b07446f
Arturocx <chris-mp@hotmail.com>
2016-10-11 19:22:26 -0700

    Repositorio Alumno

---
+++ AppTemplate/App.DAL/AlumnoRepo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.Entities;
+
+namespace App.DAL
+{
+    class AlumnoRepo
+    {
+        private AppDBContext _Contexto;
+        
+        public AlumnoRepo()
+        {
+            _Contexto = new AppDBContext();
+        }
+
+        public void Agregar(Alumno Alumnos)
+        {
+            _Contexto.Alumno.Add(Alumnos);
+            _Contexto.SaveChanges();
+        }
+        
+        public void Borrar(Alumno Alumnos)
+        {
+            _Contexto.Alumno.Remove(Alumnos);
+            _Contexto.SaveChanges();
+        }
+
+    }
+}
